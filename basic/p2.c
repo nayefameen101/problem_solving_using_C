de725fac9c369d86b273157584ffa9d1ae108a7c
nayefameen101 <nayefameen965@gmail.com>
2026-03-11 09:19:32 +0000

    Initial Commit

--- basic/p2.c
+++ basic/p2.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello Nayef\n");
+    printf("%f %f\n", 10.0, -10.9);
+    printf("%.2f %.3f\n", 10.02537, -21.67);
+    printf("%.4f", 123.00879);
+
+    printf("\n");
+    printf("\n%d %d %d %d\n\n\n", 10, 21, 70, 101);
+
+    printf("\n\n\n%d", 10);
+
+    printf("%d\n%d\n%d", 10,20, 30);
+
+    printf("\n%c %c %c\n", '1', 'T', '{');
+
+    int num = 10;
+    printf("\n%d\n", num);
+    num = 20;
+    printf("\n%d\n",num);
+
     return 0;
 }
